ea1982c1420dc9595d4bd41ccd8fbea020a04d89
Leekyungsoo <dlrudtn0924@naver.com>
2017-10-18 09:27:35 +0900

    모든 호스트 허용

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'd9@%!g5uc9b7f#kz6ivizcfvl$c&+75041@83j$@3aq39%wnp-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 
 # Application definition
